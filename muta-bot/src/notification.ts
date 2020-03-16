2beb11dd248b42c0d5674ae87e745532ad4cd2ea
Ethan <fendouhyz@163.com>
2020-03-16 20:14:48 +0800

    add telegram notification

---
+++ muta-bot/src/notification.ts
@@ -0,0 +1,27 @@
+const tg = require(`../tg`);
+import * as config from './config'
+
+import {TelegramClient} from 'messaging-api-telegram'
+
+export default function (text) {
+    if (!tg) {
+        throw new Error('cannot find telegram config file, tg.json')
+    }
+
+    const key = tg['telegram_bot_key']
+    const chat_id = config.DEV_MODE ?
+        tg['telegram_channel_id'] :
+        tg['telegram_nervos_group']
+
+    const client = TelegramClient.connect(key)
+
+    client.sendMessage(
+        chat_id,
+        text,
+        {
+            parse_mode: 'Markdown',
+            disable_web_page_preview: true,
+            disable_notification: false,
+        }
+    );
+}
